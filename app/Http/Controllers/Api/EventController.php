a0734039ffbda813c9341d828a91a6c9bd6049f1
Rafael Gomes <maciel.rafael1@gmail.com>
2023-08-25 13:22:42 -0400

    init

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index()
+    {
+        return EventResource::collection(Event::all());
+    }
+
+    public function store(Request $request)
+    {
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time'
+            ]),
+            'user_id' => 1
+        ]);
+
+        return $event;
+    }
+
+    public function show(Event $event)
+    {
+        return new EventResource($event);
+    }
+
+    public function update(Request $request, Event $event)
+    {
+        $event->update(
+            $request->validate([
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'sometimes|date',
+                'end_time' => 'sometimes|date|after:start_time'
+            ]),
+        );
+
+        return new EventResource($event);
+    }
+
+    public function destroy(Event $event)
+    {
+        $event->delete();
+
+        return response()->json([
+            'message' => 'Event deleted successfully'
+        ]);
+    }
+}
